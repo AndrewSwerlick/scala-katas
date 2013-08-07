22b96a32846c40333f7b07a5db9a7d2218c539ca
andrew <andrew.swerlick@gmail.com>
2013-08-07 00:49:04 -0400

    wiring up game next turn method

--- life/l1/src/main/scala/Game.scala
+++ life/l1/src/main/scala/Game.scala
@@ -1,5 +1,10 @@
 package swerlick.andrew.kata.life.l1
 
 class Game(startingCells:Set[Tuple2[Int,Int]]){
+	val engine = new RulesEngine()
+
 	def cells = startingCells
+	def nextTurn = {
+		new Game(startingCells.filter(ce=> engine.nextState(ce, startingCells)))
+	}
 }
--- life/l1/src/test/scala/GameSpec.scala
+++ life/l1/src/test/scala/GameSpec.scala
@@ -4,11 +4,17 @@ import org.specs2._
 import swerlick.andrew.kata.life.l1._
 
 class GameSpec extends Specification{def is = s2"""
-	Given a rules engine class it should be possible to 
+	Given a game class it should be possible to 
 		initialize a game with a series of cells = $initialize
 		retrieve starting cells from a new game  = $getCells
+	Given a game initialized with a glider pattern
+		ensure that calling nextTurn method returns the next iteration of the glider
 		"""
 
 	def initialize = new Game(Set((1,1),(1,2))) mustNotEqual(null)
 	def getCells = new Game(Set((1,1),(1,2))).cells must beEqualTo(Set((1,1),(1,2)))
+	def glider = {
+		var game = new Game(Set((1,1),(2,2),(2,3),(1,3),(0,3)))
+		game.nextTurn.cells must beEqualTo(Set((0,0),(2,0),(2,3),(1,-1),(1,-2)))	
+	}
 }
